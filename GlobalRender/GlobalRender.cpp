db73f16f0f08fafa1744113ecc2abc7bad94d3d2
Herman Gebel <gebel.german@yahoo.com>
2021-11-18 19:46:23 +0300

    Add project files.

---
+++ GlobalRender/GlobalRender.cpp
@@ -0,0 +1,12 @@
+﻿// GlobalRender.cpp : Defines the entry point for the application.
+//
+
+#include "GlobalRender.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Let's move it" << endl;
+	return 0;
+}
